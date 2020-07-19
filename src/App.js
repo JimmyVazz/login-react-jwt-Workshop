5f5ae91f475555fc5ecc21233935b6a41c609ec2
JimmyVazz <cjvasquez_98@hotmail.com>
2020-07-19 09:45:00 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import SweetAlert from 'sweetalert2-react';
+export const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    return (
+        <form>
+            <h3>Entra al portal</h3>
+
+
+            <div className="form-group">
+                <label>Email</label>
+                <input 
+                type="email" 
+                className="form-control" 
+                placeholder="Tu email" 
+                required={true}
+                />
+            </div>
+
+            <div className="form-group">
+                <label>Contraseña</label>
+                <input 
+                type="password" 
+                className="form-control" 
+                placeholder="Tu contraseña, mínimo 6 carácteres" 
+                required={true}
+                />
+            </div>
+
+            <div className="form-group">
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                    <label className="custom-control-label" htmlFor="customCheck1">Recuerdáme</label>
+                </div>
+            </div>
+
+            <button onClick={(e) => handleLogin(e)} className="btn btn-primary btn-block">Entrar</button>
+            <p className="forgot-password text-right">
+                Olvidaste tu <a href="/soporte">contraseña?</a>
+            </p>
+        </form>
+    );
+    
 }
-
-export default App;
