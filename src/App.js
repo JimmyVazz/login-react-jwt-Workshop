20bab0a2447fcfde00149d5f54c24c0f96295628
JimmyVazz <cjvasquez_98@hotmail.com>
2020-07-26 10:28:48 -0500

    se agregaron hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React, {useState} from "react";
 import SweetAlert from 'sweetalert2-react';
+
 export const App = () => {
+
+    const [email, setEmail] = useState("")  //Hook
+    const [password, setPassword] = useState("")
+
+    const handleClick = (event) => {
+        event.preventDefault()
+        console.log("Tu email:" + email)
+        console.log("Tu password es:"+ password)
+
+        //Enviar esta informacion al servidor
+    }
+
 
     
     return (
@@ -15,6 +28,7 @@ export const App = () => {
                 className="form-control" 
                 placeholder="Tu email" 
                 required={true}
+                onKeyUp={ (event) => setEmail(event.target.value) }
                 />
             </div>
 
@@ -24,6 +38,7 @@ export const App = () => {
                 type="password" 
                 className="form-control" 
                 placeholder="Tu contraseña, mínimo 6 carácteres" 
+                onKeyUp={(event) => setPassword(event.target.value)}
                 required={true}
                 />
             </div>
@@ -35,9 +50,15 @@ export const App = () => {
                 </div>
             </div>
 
-            <button onClick={(e) => handleLogin(e)} className="btn btn-primary btn-block">Entrar</button>
+            <button 
+            className="btn btn-primary btn-block"
+            onClick={(event) => handleClick(event)}
+            >Entrar
+            
+            </button>
+            
             <p className="forgot-password text-right">
-                Olvidaste tu <a href="/soporte">contraseña?</a>
+                Olvidaste tu <a href="">contraseña?</a>
             </p>
         </form>
     );
